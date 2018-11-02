943869ab45584236dbaeb2ef1a280b11851e4dfa
Netsun <303078098@qq.com>
2018-11-03 03:57:30 +0800

    修改type标签支持 参数 topid、reid

--- include/taglib/type.lib.php
+++ include/taglib/type.lib.php
@@ -1,4 +1,7 @@
-<?php   if(!defined('DEDEINC')) exit('Request Error!');
+<?php if (!defined('DEDEINC')) {
+	exit('Request Error!');
+}
+
 /**
  * 指定的单个栏目的链接标签
  *
@@ -8,7 +11,7 @@
  * @license        http://help.dedecms.com/usersguide/license.html
  * @link           http://www.dedecms.com
  */
- 
+
 /*>>dede>>
 <name>指定栏目</name>
 <type>全局标记</type>
@@ -20,65 +23,67 @@
 {/dede:type}
 </demo>
 <attributes>
-    <iterm>typeid:指定栏目ID</iterm> 
-</attributes> 
+<iterm>typeid:指定栏目ID</iterm>
+</attributes>
 >>dede>>*/
- 
-function lib_type(&$ctag,&$refObj)
-{
-    global $dsql,$envs;
 
-    $attlist='typeid|0';
-    FillAttsDefault($ctag->CAttribute->Items,$attlist);
-    extract($ctag->CAttribute->Items, EXTR_SKIP);
-    $innertext = trim($ctag->GetInnerText());
-	
-	
+function lib_type(&$ctag, &$refObj) {
+	global $dsql, $envs;
+
+	$attlist = 'typeid|0';
+	FillAttsDefault($ctag->CAttribute->Items, $attlist);
+	extract($ctag->CAttribute->Items, EXTR_SKIP);
+	$innertext = trim($ctag->GetInnerText());
+
 	/**=========================================
-	 * Author: Netsun
-	 * 获取顶级栏目
-	 */
-	if($typeid=='top'){
-		if(isset($refObj->TypeLink->TypeInfos['topid'])){
+		 * Author: Netsun
+		 * 获取顶级栏目
+	*/
+	if ($typeid == 'topid') {
+		if (isset($refObj->TypeLink->TypeInfos['topid'])) {
 			$typeid = $refObj->TypeLink->TypeInfos['topid'];
 		}
-		if($typeid==0){
-			if(isset($refObj->TypeLink->TypeInfos['id'])){
-				$typeid = $refObj->TypeLink->TypeInfos['id'];
-			}
+	} elseif ($typeid == 'reid') {
+		if (isset($refObj->TypeLink->TypeInfos['reid'])) {
+			$typeid = $refObj->TypeLink->TypeInfos['reid'];
 		}
 	}
 	/*=========================================*/
-	
 
-    if($typeid==0) {
-        $typeid = ( isset($refObj->TypeLink->TypeInfos['id']) ? $refObj->TypeLink->TypeInfos['id'] : $envs['typeid'] );
-    }
+	if ($typeid == 0) {
+		$typeid = (isset($refObj->TypeLink->TypeInfos['id']) ? $refObj->TypeLink->TypeInfos['id'] : $envs['typeid']);
+	}
 
-  if(empty($typeid)) return '';
+	if (empty($typeid)) {
+		return '';
+	}
 
-    $row = $dsql->GetOne("SELECT id,typename,typedir,isdefault,ispart,defaultname,namerule2,moresite,siteurl,sitepath 
+	$row = $dsql->GetOne("SELECT id,typename,typedir,isdefault,ispart,defaultname,namerule2,moresite,siteurl,sitepath
                           FROM `#@__arctype` WHERE id='$typeid' ");
-    if(!is_array($row)) return '';
-    if(trim($innertext)=='') $innertext = GetSysTemplets("part_type_list.htm");
-    
-    $dtp = new DedeTagParse();
-    $dtp->SetNameSpace('field','[',']');
-    $dtp->LoadSource($innertext);
-    if(!is_array($dtp->CTags))
-    {
-        unset($dtp);
-        return '';
-    }
-    else
-    {
-        $row['typelink'] = $row['typeurl'] = GetOneTypeUrlA($row);
-        foreach($dtp->CTags as $tagid=>$ctag)
-        {
-            if(isset($row[$ctag->GetName()])) $dtp->Assign($tagid,$row[$ctag->GetName()]);
-        }
-        $revalue = $dtp->GetResult();
-        unset($dtp);
-        return $revalue;
-    }
+	if (!is_array($row)) {
+		return '';
+	}
+
+	if (trim($innertext) == '') {
+		$innertext = GetSysTemplets("part_type_list.htm");
+	}
+
+	$dtp = new DedeTagParse();
+	$dtp->SetNameSpace('field', '[', ']');
+	$dtp->LoadSource($innertext);
+	if (!is_array($dtp->CTags)) {
+		unset($dtp);
+		return '';
+	} else {
+		$row['typelink'] = $row['typeurl'] = GetOneTypeUrlA($row);
+		foreach ($dtp->CTags as $tagid => $ctag) {
+			if (isset($row[$ctag->GetName()])) {
+				$dtp->Assign($tagid, $row[$ctag->GetName()]);
+			}
+
+		}
+		$revalue = $dtp->GetResult();
+		unset($dtp);
+		return $revalue;
+	}
 }
